1a5640aa07dc60aaa2d6a4b6df8b4960fefd6077
vikas <vikas22chaurasia@gmail.com>
2024-04-22 01:44:38 +0530

    finalized

--- routes/packages.js
+++ routes/packages.js
@@ -12,7 +12,7 @@ import {verifyAdmin} from "../utils/verifyToken.js"
 const router = express.Router();
 
 //CREATE
-router.post("/", verifyAdmin, createPackage);
+router.post("/", createPackage);
 
 //UPDATE
 router.put("/:id", verifyAdmin, updatePackage);
--- routes/users.js
+++ routes/users.js
@@ -22,15 +22,16 @@ const router = express.Router();
 // })
 
 //UPDATE
-router.put("/:id", verifyUser, updateUser);
+router.put("/:id", updateUser);
 
 //DELETE
-router.delete("/:id", verifyUser, deleteUser);
+router.delete("/:id", deleteUser);
 
 //GET
-router.get("/:id", verifyUser, getUser);
+//router.get("/:id", verifyUser, getUser);
+router.get("/:id", getUser);
 
 //GET ALL
-router.get("/", verifyAdmin, getUsers);
+router.get("/", getUsers);
 
 export default router;
