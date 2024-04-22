de1cde553b95072aad43120b2567e873ee15e363
Vikas22chaurasiya <96351998+Vikas22chaurasiya@users.noreply.github.com>
2024-04-22 09:54:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ mongoose.connection.on("disconnected", () => {
 
 //middlewares
 app.use(cors())
+
+app.use((req, res, next) => {
+  // Add custom headers here
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(cookieParser())
 app.use(express.json());
 
