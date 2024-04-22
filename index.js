048987cf82e67c18cd8ae20a7a2afcb0d1966302
Vikas22chaurasiya <96351998+Vikas22chaurasiya@users.noreply.github.com>
2024-04-22 11:52:46 +0530

    Update index.js aded cororigins

--- index.js
+++ index.js
@@ -24,17 +24,13 @@ const connect = async () => {
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected!");
 });
+const allowedOrigins = ['https://tour-app-c9qv.onrender.com'];
+app.use(cors({
+  origin: allowedOrigins
+}));
 
-//middlewares
-app.use(cors())
 
-app.use((req, res, next) => {
-  // Add custom headers here
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 
 app.use(cookieParser())
 app.use(express.json());
